f18c5a5149310fcbd24e5f3f8c0e87d41affe5ef
billyi <15709380+billyi01@users.noreply.github.com>
2022-05-30 11:48:54 +0800

    Update init.r

--- init.r
+++ init.r
@@ -1,3 +1,15 @@
+################################################################################
+#
+# "devtools" |> 
+#   (function(x) x[!(x %in% rownames(installed.packages()))])() |>
+#   lapply(\(x) install.packages(x, dependencies = TRUE)) |>
+#   invisible()
+# 
+# "https://raw.githubusercontent.com/billyi01/Ri/main/init.r" |>
+#   devtools::source_url()
+#
+################################################################################
+
 rm(list = ls())
 gc()
 options(scipen=999)
