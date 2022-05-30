60f378efc26cd054ac4024b0ea507b13e75f5c55
billyi <15709380+billyi01@users.noreply.github.com>
2022-05-30 20:23:21 +0800

    Update and rename instpkgs.r to instPkgs.r

--- instPkgs.r
+++ instPkgs.r
@@ -1,4 +1,4 @@
-instpkgs <- function(x) {
+instPkgs <- function(x) {
   x |> 
     (\(x) x[!(x %in% rownames(installed.packages()))])() |>
     lapply(\(x) install.packages(x, dependencies = TRUE)) |>
